b0ebbd14215fda603cda88b805e6b77bee101ab1
Kindyl <kingkindyl@gmail.com>
2022-09-05 14:36:32 -0400

    updated neopixel instructions

--- book/labs/1_Color_Labs/1_Color_Vector/neoexample.py
+++ book/labs/1_Color_Labs/1_Color_Vector/neoexample.py
@@ -3,8 +3,6 @@ This is a demo of the Neopixels, using a library published at
 https://github.com/blaz-r/pi_pico_neopixel
 """
 
-
-
 '''
 The key methods are set_pixel(n (r,g,b)), 
 set_pixel_line(p1, p2, (r, g, b)) which sets a row of pixels from pixel p1 to pixel p2 (inclusive),
@@ -13,9 +11,17 @@ and fill((r,g,b)) which fills all the pixels with the color r, g, b.
 
 import time
 from neopixel import Neopixel
+from machine import Pin
 
+# turn the pico's onboard LED on for 1 second and then off
+led = Pin(25, Pin.OUT)
+led.toggle()
+time.sleep(1)
+led.off()
+
+# displays looping sequence on 6 neopixels
 numpix = 6
-strip = Neopixel(numpix, 0, 0,"GRB")
+strip = Neopixel(numpix, 0, 0)
 
 red = (255, 0, 0)
 orange = (255, 165, 0)
